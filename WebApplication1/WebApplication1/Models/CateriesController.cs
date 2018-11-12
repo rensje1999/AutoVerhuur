35c5d1eeb6255af1c9a3b04405085b69563b6f4c
Rens Koster <renskoster99@gmail.com>
2018-11-12 18:40:20 +0100

    push porject

---
+++ WebApplication1/WebApplication1/Models/CateriesController.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Models
+{
+    public class CateriesController : Controller
+    {
+        private Voertuigen_Verhuur_JansenEntities db = new Voertuigen_Verhuur_JansenEntities();
+
+        // GET: Cateries
+        public ActionResult Index()
+        {
+            return View(db.Cateries.ToList());
+        }
+
+        // GET: Cateries/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Caterie caterie = db.Cateries.Find(id);
+            if (caterie == null)
+            {
+                return HttpNotFound();
+            }
+            return View(caterie);
+        }
+
+        // GET: Cateries/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Cateries/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "caterieId,caterieNaam,aantalPersonen,aantalKoffers")] Caterie caterie)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Cateries.Add(caterie);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(caterie);
+        }
+
+        // GET: Cateries/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Caterie caterie = db.Cateries.Find(id);
+            if (caterie == null)
+            {
+                return HttpNotFound();
+            }
+            return View(caterie);
+        }
+
+        // POST: Cateries/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "caterieId,caterieNaam,aantalPersonen,aantalKoffers")] Caterie caterie)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(caterie).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(caterie);
+        }
+
+        // GET: Cateries/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Caterie caterie = db.Cateries.Find(id);
+            if (caterie == null)
+            {
+                return HttpNotFound();
+            }
+            return View(caterie);
+        }
+
+        // POST: Cateries/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Caterie caterie = db.Cateries.Find(id);
+            db.Cateries.Remove(caterie);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ WebApplication1/WebApplication1/Models/VoertuigensController.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Models
+{
+    public class VoertuigensController : Controller
+    {
+        private Voertuigen_Verhuur_JansenEntities db = new Voertuigen_Verhuur_JansenEntities();
+
+        // GET: Voertuigens
+        public ActionResult Index()
+        {
+            var voertuigens = db.Voertuigens.Include(v => v.Caterie);
+            return View(voertuigens.ToList());
+        }
+
+        // GET: Voertuigens/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Voertuigen voertuigen = db.Voertuigens.Find(id);
+            if (voertuigen == null)
+            {
+                return HttpNotFound();
+            }
+            return View(voertuigen);
+        }
+
+        // GET: Voertuigens/Create
+        public ActionResult Create()
+        {
+            ViewBag.caterieId = new SelectList(db.Cateries, "caterieId", "caterieNaam");
+            return View();
+        }
+
+        // POST: Voertuigens/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "kenteken,caterieId,merk,type")] Voertuigen voertuigen)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Voertuigens.Add(voertuigen);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.caterieId = new SelectList(db.Cateries, "caterieId", "caterieNaam", voertuigen.caterieId);
+            return View(voertuigen);
+        }
+
+        // GET: Voertuigens/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Voertuigen voertuigen = db.Voertuigens.Find(id);
+            if (voertuigen == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.caterieId = new SelectList(db.Cateries, "caterieId", "caterieNaam", voertuigen.caterieId);
+            return View(voertuigen);
+        }
+
+        // POST: Voertuigens/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "kenteken,caterieId,merk,type")] Voertuigen voertuigen)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(voertuigen).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.caterieId = new SelectList(db.Cateries, "caterieId", "caterieNaam", voertuigen.caterieId);
+            return View(voertuigen);
+        }
+
+        // GET: Voertuigens/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Voertuigen voertuigen = db.Voertuigens.Find(id);
+            if (voertuigen == null)
+            {
+                return HttpNotFound();
+            }
+            return View(voertuigen);
+        }
+
+        // POST: Voertuigens/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            Voertuigen voertuigen = db.Voertuigens.Find(id);
+            db.Voertuigens.Remove(voertuigen);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
