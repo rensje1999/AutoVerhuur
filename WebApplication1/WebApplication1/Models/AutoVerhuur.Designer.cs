6280aec20c35497fadb4611d3a8fa864b1e59f82
Rens Koster <renskoster99@gmail.com>
2018-11-12 19:50:29 +0100

    website is geupgrade.

--- WebApplication1/WebApplication1/Models/AutoVerhuur.Designer.cs
+++ WebApplication1/WebApplication1/Models/AutoVerhuur.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\Rens\source\repos\WebApplication1\WebApplication1\Models\AutoVerhuur.edmx'. 
+﻿// T4 code generation is enabled for model 'D:\Github\AutoVerhuur\WebApplication1\WebApplication1\Models\AutoVerhuur.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
